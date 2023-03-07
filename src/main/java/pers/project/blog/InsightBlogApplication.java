fff80dda8f41a3901157cc5c52315011e4ef7ecd
Luo Fei <790354953@qq.com>
2023-03-07 11:16:52 +0800

    日志检查时区和字符集

--- src/main/java/pers/project/blog/InsightBlogApplication.java
+++ src/main/java/pers/project/blog/InsightBlogApplication.java
@@ -1,7 +1,11 @@
 package pers.project.blog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.charset.Charset;
+import java.time.ZoneId;
 
 /**
  * 主启动类
@@ -9,10 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Luo Fei
  * @date 2022-12-22
  */
+@Slf4j
 @SpringBootApplication
 public class InsightBlogApplication {
 
     public static void main(String[] args) {
+        log.info("系统默认时区 {}，可使用 JVM 参数 -Duser.timezone=GMT+08:00 修改",
+                ZoneId.systemDefault());
+        log.info("系统默认字符集 {}，可使用 JVM 参数 -Dfile.encoding=UTF-8 修改",
+                Charset.defaultCharset());
         SpringApplication.run(InsightBlogApplication.class, args);
     }
 
